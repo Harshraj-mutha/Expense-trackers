fc41e9fecfaf38f8ae4d450d30c1139f1801b5d1
Harshraj-mutha <harshmutha333@gmail.com>
2025-03-26 11:55:06 +0530

    Category code

---
+++ Expense-tracker/src/main/java/com/app/entity/Category.java
@@ -0,0 +1,21 @@
+package com.app.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "category")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Category extends BaseEntity {
+	
+	private String category;
+	private String description;
+
+}
